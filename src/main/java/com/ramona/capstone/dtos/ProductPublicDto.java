ee116cb920e3f0aa7f43008f779fc9aec7d3d3ec
DevRamona <r.ingabire@alustudent.com>
2026-01-13 09:36:44 +0200

    chore: add the status stock

---
+++ src/main/java/com/ramona/capstone/dtos/ProductPublicDto.java
@@ -0,0 +1,12 @@
+package com.ramona.capstone.dtos;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ProductPublicDto {
+    private String name;
+    private String categoryName;
+    private List<VariantPublicDto> variants;
+}
